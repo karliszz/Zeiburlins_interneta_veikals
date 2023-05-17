d610cdd38154c8ce31409252ba72e1b8dff633a0
karliszz <karliszeibis@gmail.com>
2023-05-17 16:51:31 +0300

    Izveidota ielogosanas funkcija

--- login.php
+++ login.php
@@ -11,6 +11,36 @@
    <div class="form-container">
       <form action="" method="post">
          <h3>Ielogošanās sistēmā</h3>
+
+         <?php 
+					if(isset($_POST['submit'])){
+						require("connection.php");
+						session_start();
+						$Lietotajvards = mysqli_real_escape_string($savienojums, $_POST['email']);
+						$Parole = mysqli_real_escape_string($savienojums, $_POST['password']);
+						$sqlVaicajums = "SELECT * FROM lietotaji WHERE lietotaja_vards = '$Lietotajvards'";
+						$rezultats = mysqli_query($savienojums, $sqlVaicajums); 
+
+						if(mysqli_num_rows($rezultats) == 1){
+							while($row = mysqli_fetch_array($rezultats)){
+								if(password_verify($Parole, $row["Parole"])){
+									$_SESSION["username"] = $Lietotajvards;
+									header("location:index.php");
+								}else{
+									echo "Nepareizs E-pasts vai Parole!";
+								}
+							}
+						}else{
+							echo "Nepareizs E-pasts vai Parole!";
+						}
+					}
+
+					if(isset($GET['logout'])){
+						session_destroy();
+					}
+		
+				?>
+
          <input type="email" name="email" required placeholder="Ievadiet savu e-pastu">
          <input type="password" name="password" required placeholder="Ievadiet savu paroli">
          <input type="submit" name="submit" value="Ielogoties" class="form-btn">
