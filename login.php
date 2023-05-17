86b052c8ed71afa5a894f2b50acf178cbb850dd0
karliszz <karliszeibis@gmail.com>
2023-05-17 15:54:08 +0300

    Uzlabots login un css fails

--- login.php
+++ login.php
@@ -6,24 +6,15 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Ielogošanās lapa</title>
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
-<div class="form-container">
-
-   <form action="" method="post">
-      <h3>Ielogošanās sistēmā</h3>
-
-
-      
-      <input type="email" name="email" required placeholder="Ievadiet savu e-pastu">
-      <input type="password" name="password" required placeholder="Ievadiet savu paroli">
-      <input type="submit" name="submit" value="Ielogoties" class="form-btn">
-      
-   </form>
-
-</div>
-
+   <div class="form-container">
+      <form action="" method="post">
+         <h3>Ielogošanās sistēmā</h3>
+         <input type="email" name="email" required placeholder="Ievadiet savu e-pastu">
+         <input type="password" name="password" required placeholder="Ievadiet savu paroli">
+         <input type="submit" name="submit" value="Ielogoties" class="form-btn">
+      </form>
+   </div>
 </body>
 </html>
