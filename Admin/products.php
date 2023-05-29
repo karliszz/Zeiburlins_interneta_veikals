e95ceb93b312c8d8b537d708e688a23e4025faf1
karliszz <karliszeibis@gmail.com>
2023-05-29 10:40:01 +0300

    Izveidoat produktu sadala

---
+++ Admin/products.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Admin Panel</title>
+   <link rel="stylesheet" href="../CSS/admin.css">
+</head>
+<body>
+   <header>
+      <nav>
+         <ul>
+            <li><a href="index.php">Home</a></li>
+            <li id="active"><a href="products.php">Products</a></li>
+            <li><a href="orders.php">Orders</a></li>
+            <li><a href="users.php">Users</a></li>
+            <li><a href="logout.php">Logout</a></li>
+         </ul>
+      </nav>
+   </header>
+
+   <div class="container">
+      <h1>Produkti</h1>
+      <!-- Šeit bus funkcija kura uzradīs visus produktus -->
+   </div>
+
+   <footer>
+      <p>&copy; 2023 Your Company. All rights reserved.</p>
+   </footer>
+</body>
+</html>
