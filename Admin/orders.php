ff1341616417b69a23ab5378d7f004eaac97060d
karliszz <karliszeibis@gmail.com>
2023-05-29 10:43:47 +0300

    izveidota pasutijumu sadala

---
+++ Admin/orders.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Admin Panel</title>
+   <link rel="stylesheet" href="../CSS/admin.css">
+</head>
+<body>
+   <header>
+      <nav>
+         <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="products.php">Produkti</a></li>
+            <li id="active"><a href="orders.php">Pasūtījumi</a></li>
+            <li><a href="logout.php">Izlogoties</a></li>
+         </ul>
+      </nav>
+   </header>
+
+   <div class="container">
+      <h1>Pasūtījumi </h1>
+      <!-- Šeit bus funkcija kura uzradīs visus pasutijumus -->
+   </div>
+
+   <footer>
+      <p>&copy; 2023 Your Company. All rights reserved.</p>
+   </footer>
+</body>
+</html>
