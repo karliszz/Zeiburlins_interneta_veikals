aebfaf5ee27efc533ae96e49ca3309fa65868156
karliszz <karliszeibis@gmail.com>
2023-05-29 10:03:06 +0300

    izveido admin panela index lapu

---
+++ Admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Admin Panel</title>
+   <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+   <header>
+      <nav>
+         <ul>
+            <li><a href="../index.php">Galvenā lapa</a></li>
+            <li><a href="products.php">Produkti</a></li>
+            <li><a href="orders.php">Pasutijumi</a></li>
+            <li><a href="logout.php">Izlogoties</a></li>
+         </ul>
+      </nav>
+   </header>
+
+   <div class="container">
+      Sveicināts admin sadaļā!
+   </div>
+
+   <footer>
+      <p>&copy; 2023 Your Company. All rights reserved.</p>
+   </footer>
+</body>
+</html>
