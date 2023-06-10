894dfc23c2cb37b26d8350d5eb0e543a62d3af33
karliszz <karliszeibis@gmail.com>
2023-06-10 17:43:49 +0300

    Pasutijuma lapas izveide

---
+++ pasutijums.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Blossom Beauty</title>
+   <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+<header>
+      <nav>
+         <ul>
+            <li><a href="index.php">Galvenā</a></li>
+            <li><a href="index.php">Par mums</a></li>
+            <li><a href="index.php">Katalogs</a></li>
+            <li><a href="index.php">Pakalpojumi</a></li>
+            <li><a href="index.php">Ieraksti</a></li>
+            <li><a href="login.php">Ielogoties</a></li>
+         </ul>
+      </nav>
+   </header>
+
+   <footer>
+      <div class="container">
+         <p>&copy; 2023 Blossom Beauty. All rights reserved.</p>
+      </div>
+   </footer>
+
+</body>
+</html>
